5dfdfd0f40c5aa27b186715c1d2f527aef304f01
Peter Karman <karpet@peknet.com>
2016-02-01 09:18:31 -0600

    prep,release 0.08

--- lib/SWISH/HiLiter.pm
+++ lib/SWISH/HiLiter.pm
@@ -16,7 +16,7 @@ if ( $@
     die "SWISH::HiLiter requires SWISH::API version 0.03 or newer\n";
 }
 
-our $VERSION = '0.07';
+our $VERSION = '0.08';
 
 __PACKAGE__->mk_accessors(qw( hiliter snipper swish query ));
 
